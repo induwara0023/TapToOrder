a4cae576b9b46f48aa1a1fa142fb8ed4880e22dc
Induwara Surasinghe <170860970+induwara0023@users.noreply.github.com>
2025-10-21 14:39:05 +0530

    Update WebSocket URL to ngrok endpoint

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -46,8 +46,7 @@ import { Inject, PLATFORM_ID } from '@angular/core';
 export class WebsocketService {
   private socket$: WebSocketSubject<any> | null = null;
   // private readonly WS_URL = 'ws://localhost:4000';
-  // private readonly WS_URL = 'wss://www.shopynits.shop:4000';
-  private readonly WS_URL = 'ws://taptoorder-websocket.onrender.com';
+  private readonly WS_URL = 'wss://https://samson-tourmalinic-unnaggingly.ngrok-free.dev';
 
 
   constructor(@Inject(PLATFORM_ID) private platformId: any) {
